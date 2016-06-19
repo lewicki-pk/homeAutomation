21fcf9433b8a0556f0bc4dcff1f21ccc3d4ec6f7
lewiatan <lewicki.pk@gmail.com>
2016-06-19 20:20:37 +0200

    read data from DHT11 connected to rpi

--- src/homeAutomation.cpp
+++ src/homeAutomation.cpp
@@ -1,25 +1,33 @@
-#include "Controller.h"
+//#include "Controller.h"
 
 //#include <CommonMessages.hpp>
+#include "PiDht.h"
 
 #include <iostream>
 #include <boost/asio.hpp>
+#include <functional>
 
 int main()
 {
     boost::asio::io_service ioService;
+
     boost::asio::deadline_timer timer(ioService);
-    timer.expires_from_now(boost::posix_time::seconds(5));
-    timer.async_wait([&](const boost::system::error_code&)
-            {std::cout << "Timer expired" << std::endl;});
+    timer.expires_from_now(boost::posix_time::seconds(1));
+
+    std::function<void(const boost::system::error_code&)> myFunctor = [](const boost::system::error_code&)
+        {
+            PiDht sensor;
+            sensor.readOne();
+            if (sensor.getReadStatus()) {
+                std::cout << "Received readings. Temperature = " << sensor.getTemperature() << " degrees Celsius, "
+                          << "Humidity = " << sensor.getHumidity() << " %" << std::endl;
+            } else {
+                std::cout << "Unable to read status" << std::endl;
+            }
+        };
+
+    timer.async_wait(myFunctor);
     std::cout << "Starting timer" << std::endl;
     ioService.run();
-//    try {
-//        controller.execute();
-//    }
-//    catch (const std::exception& e) {
-//        std::cerr << "Exception: " << e.what() << std::endl;
-//        return 1;
-//    }
     return 0;
 }
