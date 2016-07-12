0488bed25ca3fb805a68a052bc93965e71642bf3
lewiatan <lewicki.pk@gmail.com>
2016-07-12 21:17:20 +0200

    remove unused files definitely

--- unused_includes/DebugLog.hpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-#ifdef DEBUG 
-#define DEBUG_LOG(x) (std::cout << "[" << __PRETTY_FUNCTION__ << ":" << __LINE__ << "]" << (x) << std::endl)
-#define D(x) x
-#else 
-#define DEBUG_LOG(x)
-#define D(x)
-#endif
